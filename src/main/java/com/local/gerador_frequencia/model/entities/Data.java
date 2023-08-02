a91351fa3115c6e1cdfb51419f8d1aa6d98bc160
joaoAaf <joao.anderson.af@gmail.com>
2023-08-01 22:51:49 -0300

    Refatoração classe Data

--- src/main/java/com/local/gerador_frequencia/model/entities/Data.java
+++ src/main/java/com/local/gerador_frequencia/model/entities/Data.java
@@ -1,8 +1,9 @@
 package com.local.gerador_frequencia.model.entities;
 
-import java.util.Date;
-
-import org.springframework.format.annotation.DateTimeFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Data {
 
@@ -12,15 +13,13 @@ public class Data {
     private String funcao;
     private String lotacao;
     private String setor;
-    @DateTimeFormat(pattern = "yyyy-MM")
-    private Date mes;
+    private String mes;
+    private String ano;
     private Integer jornada;
-    @DateTimeFormat(pattern = "HH:mm")
-    private Date horarioEntrada;
-    @DateTimeFormat(pattern = "HH:mm")
-    private Date horarioSaida;
+    private List<String> horarios = new ArrayList<>();
     private Boolean repetir;
     private Boolean aleatorio;
+    private List<LocalDateTime> dia = new ArrayList<>();
     
     public Data() {
     }
@@ -28,74 +27,96 @@ public class Data {
     public String getNome() {
         return nome;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getMatricula() {
         return matricula;
     }
+
     public void setMatricula(String matricula) {
         this.matricula = matricula;
     }
+
     public String getCargo() {
         return cargo;
     }
+
     public void setCargo(String cargo) {
         this.cargo = cargo;
     }
+
     public String getFuncao() {
         return funcao;
     }
+
     public void setFuncao(String funcao) {
         this.funcao = funcao;
     }
+
     public String getLotacao() {
         return lotacao;
     }
+
     public void setLotacao(String lotacao) {
         this.lotacao = lotacao;
     }
+
     public String getSetor() {
         return setor;
     }
+
     public void setSetor(String setor) {
         this.setor = setor;
     }
-    public Date getMes() {
-        return mes;
-    }
-    public void setMes(Date mes) {
+
+    public void setMes(String mes) {
         this.mes = mes;
     }
+
+    public void setAno(String ano) {
+        this.ano = ano;
+    }
+
     public Integer getJornada() {
         return jornada;
     }
+
     public void setJornada(Integer jornada) {
         this.jornada = jornada;
     }
-    public Date getHorarioEntrada() {
-        return horarioEntrada;
+
+    public void setHorarios(String horario) {
+        this.horarios.add(horario);
     }
-    public void setHorarioEntrada(Date horarioEntrada) {
-        this.horarioEntrada = horarioEntrada;
-    }
-    public Date getHorarioSaida() {
-        return horarioSaida;
-    }
-    public void setHorarioSaida(Date horarioSaida) {
-        this.horarioSaida = horarioSaida;
-    }
+
     public Boolean getRepetir() {
         return repetir;
     }
+
     public void setRepetir(Boolean repetir) {
         this.repetir = repetir;
     }
+
     public Boolean getAleatorio() {
         return aleatorio;
     }
+
     public void setAleatorio(Boolean aleatorio) {
         this.aleatorio = aleatorio;
+    }
+
+    public List<LocalDateTime> getDia() {
+        return dia;
+    }
+
+    public void setDia() {
+        for (int i = 0; i < this.horarios.size(); i++) {
+            String dia = this.mes+this.ano+this.horarios.get(i);
+            this.dia.add(LocalDateTime.parse(dia,DateTimeFormatter.ofPattern("MMyyyyHH:mm")));
+        }
     }
     
 }
