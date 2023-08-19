5041884916612a6484f91f0d70b3f5ab7d30acec
joaoAaf <joao.anderson.af@gmail.com>
2023-08-18 22:40:40 -0300

    Adição do alert Toast para o botão Copiar HTML

--- src/main/resources/static/js/createHTML.js
+++ src/main/resources/static/js/createHTML.js
@@ -1,3 +1,9 @@
+function alerta() {
+    const toastLive = document.getElementById('liveToast')
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLive)
+    toastBootstrap.show()
+}
+
 function geraHtml() {
     let frequency = document.getElementById('frequency')
     let stringHtml = frequency.innerHTML
